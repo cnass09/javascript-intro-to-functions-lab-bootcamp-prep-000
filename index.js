a136e2652b020d0aba158c5e9c1dfb4587aaaa4d
Calista Nasser <nasser.calista@gmail.com>
2017-12-12 19:03:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logshout(string) {
-  return string.toUpperCase()
+function logShout(string) {
+  console.log(string).toUpperCase()
 }
