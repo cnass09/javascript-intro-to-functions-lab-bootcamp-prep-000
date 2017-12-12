5928a7dd85156f18389a9c2da4a561e04434e43f
Calista Nasser <nasser.calista@gmail.com>
2017-12-12 19:29:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
+  var uppercase = string.toUpperCase
+  var lowercase = string.toLowerCase
   if (string == "I love you, Grandma.") {
     return "I love you, too."
-  } else if (string.toUpperCase == string) {
+  } else if (string == uppercase) {
     return "YES INDEED!"
-  } else if (string == string.toLowerCase) {
+  } else if (string == lowercase) {
     return "I can't hear you!"
   }
 }
